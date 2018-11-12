53d23231ec553bffdf151b242c696d92b0ba69bd
Tsai <xs19283@gmail.com>
2018-11-12 20:34:14 +0800

    Receive C#V1

---
+++ DataTransfor1021/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace DataTransfor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.xAxis = new HslCommunication.Controls.UserCurve();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.startObservation = new HslCommunication.Controls.UserButton();
+            this.HallText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // xAxis
+            // 
+            this.xAxis.BackColor = System.Drawing.Color.Transparent;
+            this.xAxis.Location = new System.Drawing.Point(108, 237);
+            this.xAxis.Name = "xAxis";
+            this.xAxis.Size = new System.Drawing.Size(589, 197);
+            this.xAxis.TabIndex = 0;
+            this.xAxis.ValueMaxLeft = 3000F;
+            this.xAxis.ValueMaxRight = 3000F;
+            this.xAxis.ValueMinLeft = 1000F;
+            this.xAxis.ValueMinRight = 1000F;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F);
+            this.label1.Location = new System.Drawing.Point(132, 215);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 19);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "X軸數值";
+            // 
+            // startObservation
+            // 
+            this.startObservation.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.startObservation.BackColor = System.Drawing.Color.Transparent;
+            this.startObservation.CustomerInformation = "";
+            this.startObservation.EnableColor = System.Drawing.Color.FromArgb(((int)(((byte)(190)))), ((int)(((byte)(190)))), ((int)(((byte)(190)))));
+            this.startObservation.Font = new System.Drawing.Font("微软雅黑", 12F);
+            this.startObservation.Location = new System.Drawing.Point(619, 691);
+            this.startObservation.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.startObservation.Name = "startObservation";
+            this.startObservation.Size = new System.Drawing.Size(78, 25);
+            this.startObservation.TabIndex = 8;
+            this.startObservation.UIText = "開始觀測";
+            this.startObservation.Click += new System.EventHandler(this.StartObservation_Click);
+            // 
+            // HallText
+            // 
+            this.HallText.AllowDrop = true;
+            this.HallText.Location = new System.Drawing.Point(761, 257);
+            this.HallText.MaxLength = 100000000;
+            this.HallText.Multiline = true;
+            this.HallText.Name = "HallText";
+            this.HallText.ReadOnly = true;
+            this.HallText.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.HallText.Size = new System.Drawing.Size(386, 371);
+            this.HallText.TabIndex = 9;
+            this.HallText.TextChanged += new System.EventHandler(this.HallText_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F);
+            this.label2.Location = new System.Drawing.Point(757, 215);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "輪速感測器";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 20F);
+            this.label3.Location = new System.Drawing.Point(167, 514);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 27);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("新細明體", 20F);
+            this.label4.Location = new System.Drawing.Point(307, 514);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 27);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(1350, 729);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.HallText);
+            this.Controls.Add(this.startObservation);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.xAxis);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "數值監控";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private HslCommunication.Controls.UserCurve xAxis;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label1;
+        private HslCommunication.Controls.UserButton startObservation;
+        private System.Windows.Forms.TextBox HallText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ DataTransfor1021/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Text;
+using System.Threading;
+using System.Linq;
+using System.Windows.Forms;
+using Timer = System.Windows.Forms.Timer;
+using System.IO;
+using System.IO.Ports;
+
+namespace DataTransfor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int[] AxisModBusData = new int[4];
+        ushort[] HallModBusData = new ushort[4];
+        ushort HallData;
+        int AxisData;
+        int count;
+
+        private Boolean receiving;
+        private SerialPort comport;
+        private Thread t;
+
+        delegate void Display(Byte[] buffer);
+
+        ////////IO文字
+        private void WriteTimeData()
+        {
+            StreamWriter sw = new StreamWriter(@"D:\Users\IIIT\Desktop\Data.txt", true);
+            DateTime saveNow = DateTime.Now;
+            //sw.WriteLine(saveNow.ToString("hh:mm:ss.ff") + " 三軸數值 : " + AxisData + " 輪速感測器 : " + HallData);
+            sw.WriteLine(AxisData);
+            sw.Close();
+        }
+
+        ////////程式一執行做藍芽serialPort初始化, 以及曲線圖初始化
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SerialPortInit(sender, e);
+            InitGraph(sender, e);
+        }
+
+        ////////SerialPort初始化  (確認 COM PORT 有無正確)
+        private void SerialPortInit(object sender, EventArgs e)
+        {
+            comport = new SerialPort("COM10", 9600, Parity.None, 8, StopBits.One);
+            if (!comport.IsOpen)
+            {
+                comport.Open();
+                receiving = true;
+                t = new Thread(DoReceive);
+                t.IsBackground = true;
+                t.Start();
+            }
+        }
+
+        ////////曲線圖初始化
+        private void InitGraph(object sender, EventArgs e)
+        {
+            xAxis.SetLeftCurve("A", new float[] { }, Color.Tomato);
+        }
+
+        ////////X軸資料顯示
+        private void XAxisValueShow(object sender, EventArgs e)
+        {
+            Timer timer = new Timer
+            {
+                Interval = 50  //10毫秒更新一次
+            };
+            timer.Tick += (sender1, e1) =>
+            {
+                xAxis.AddCurveData("A", AxisData);
+            };
+            timer.Start();
+        }
+
+
+        ////////開始觀測
+        private void StartObservation_Click(object sender, EventArgs e)
+        {
+            XAxisValueShow(sender, e);
+        }
+
+        private void ThreadText()
+        {
+            String data = Convert.ToString(HallData);
+            SetText(HallText.Text + "\t" + data);
+        }
+
+        delegate void SetTextCallback(string text);
+        private void SetText(String text)
+        {
+            if (this.HallText.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.HallText.Text = text;
+            }
+        }
+        private void DoReceive()
+        {
+            Boolean ctrl = true;
+            while (ctrl)
+            {
+                int b = comport.ReadByte();
+                if (b == 3)
+                {
+                    for (int i = 0; i < 9; i++)
+                    {
+                        comport.ReadByte();
+                    }
+                    ctrl = false;
+                }
+            }
+            Byte[] buffer = new Byte[5];
+            while (receiving)
+            {
+                while (comport.BytesToRead < 5)
+                {
+                    //Thread.Sleep(8);
+                }
+                Int32 length = comport.Read(buffer, 0, buffer.Length);
+                if (buffer[0] == 3 && buffer[0] + buffer[1] == buffer[4])
+                {
+                    AxisData = (ushort)(buffer[1] << 8);
+                    AxisData += buffer[2];
+                    count = buffer[3];
+                }
+
+                if (buffer[0] == 85 && buffer[0] + buffer[1] == buffer[4])
+                {
+                    HallData = (ushort)(buffer[1] << 8);
+                    HallData += buffer[2];
+                }
+                WriteTimeData();
+                ThreadText();
+                //Thread.Sleep(16);
+
+            }
+        }
+        private void HallText_TextChanged(object sender, EventArgs e)
+        {
+            this.HallText.SelectionStart = this.HallText.Text.Length;
+            this.HallText.ScrollToCaret();
+        }
+    }
+}
+
+
+//sbyte CRC;
+//byte sum;
+//SerialPort sp = (SerialPort)sender;
+// if (sp.BytesToRead >= 5)
+// {
+//   byte[] bufferData = new byte[sp.BytesToRead];
+//   sp.Read(bufferData, 0, bufferData.Length);
+
+//  if (bufferData[0] == 3 && bufferData[0] + bufferData[1] == bufferData[4])
+// {
+/*for (int i = 1; i < 4; i++)
+{
+    AxisModBusData[i - 1] = bufferData[i];
+}*/
+// sum = Convert.ToByte(AxisModBusData[0] + 3);
+//  CRC = Convert.ToSByte((sbyte)~sum);
+//   CRC++;
+// if (CRC == AxisModBusData[1021])
+//{
+//AxisData = (ushort)(bufferData[1] << 8);
+// AxisData += bufferData[2];
+// count = bufferData[3];
+//}
+// }
+
+//if (bufferData[0] == 2 && bufferData[0] + bufferData[1] == bufferData[4])
+//{
+/*for (int i = 1; i < 4; i++)
+{
+    HallModBusData[i - 1] = bufferData[i];
+}*/
+//sum = Convert.ToByte(HallModBusData[0] + 2);
+//CRC = ~sum + 1;
+//if (CRC == HallModBusData[1])
+//{
+//    HallData = bufferData[1];
+//}
+//HallData = (ushort)(bufferData[1] << 8);
+// HallData += bufferData[2];
+//  }
+// }
+// }
+// WriteTimeData();
+//ThreadText();
+
+
+
+
+
+
